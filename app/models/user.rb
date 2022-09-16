a38c1e27f64225111fb0be76a0146b82a4ab9317
Luke Loera <lgloera2023@gmail.com>
2022-09-16 11:56:22 -0500

    Add user and user_books entities

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+    
+    has_many :books, through: :user_books
+    has_many :user_books
+    
+    validates :username, presence: true, uniqueness: { case_sensitive: false }
+    
+end
